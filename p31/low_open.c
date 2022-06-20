f04a61c62d308a45c58ca948043b0de1109e6c88
yumdari <epqmfzkdlwj@naver.com>
2022-06-20 19:17:17 +0900

    220620

---
+++ p31/low_open.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+void error_handling(char* message);
+
+int main(void)
+{
+	int fd;
+	char buf[]="Let's go!\n";
+
+	fd = open("data.txt", O_CREAT|O_WRONLY|O_TRUNC);
+	if(fd==-1)
+		error_handling("open() error!");
+	printf("file descriptor : %d \n", fd);
+
+	if(write(fd, buf, sizeof(buf))==-1)
+		error_handling("write() error!");
+	close(fd);
+	return 0;
+}
+
+void error_handling(char* message)
+{
+	// 이전 예제와 동일하므로 생략!
+}
