fabe573bd42ba8d5577a9b80bcf9fb135fe115d7
yumdari <epqmfzkdlwj@naver.com>
2022-06-23 15:35:36 +0900

    220623

---
+++ makefile/input.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char* input() {
+
+    char * str;
+    str = (char*)malloc(BUFSIZ);
+    scanf("%s", str);
+
+    return str;
+}
---
+++ makefile/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+char* input();
+void print(char*);
+
+int main()
+{
+	char* str = input();
+	print(str);
+		print("\n");
+
+	return 0;
+}
